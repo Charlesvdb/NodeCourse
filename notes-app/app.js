9fe93f305c0e5e078150532dea1aa8f5bb8b5fb0
Charles Van den Bergh <charles.vandenbergh@panenco.com>
2020-08-28 13:46:02 +0200

    done

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,1 +1,11 @@
-fs.writeFileSync("notes.txt","This file was created by Node.js")
+// const fs = require("fs")
+
+// // fs.writeFileSync("notes.txt","My name is Charles")
+
+// fs.appendFileSync("notes.txt","Adding this line to the core")
+
+const getNotes = require('./notes.js')
+
+const msg = getNotes()
+
+console.log(msg)

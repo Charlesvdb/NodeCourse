06e0e8f53f773ebc0e1461caf7cf5087b9c04040
Charles Van den Bergh <charles.vandenbergh@panenco.com>
2020-09-01 19:18:09 +0200

    done

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,7 +4,7 @@ const url = "http://api.weatherstack.com/current?access_key=13ce70dbdb8a262b03f1
 
 request({url: url, json: true}, (error,response) => {
     // console.log(response.body.current)
-    console.log("It is currently " + response.body.current.temperature + " degress out. It feels like " + response.body.current.feelslike) +  "degress out"
+    console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out. It feels like " + response.body.current.feelslike) +  "degress out"
 })
 
 

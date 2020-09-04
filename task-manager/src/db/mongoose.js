e46b4356a8da03fd255616e33ec0ab395c6dcffc
Charles Van den Bergh <charles.vandenbergh@panenco.com>
2020-09-04 15:52:36 +0200

    done

---
+++ task-manager/src/db/mongoose.js
@@ -0,0 +1,81 @@
+const mongoose = require("mongoose")
+const validator = require("validator")
+
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+
+// const User = mongoose.model("User",{
+//     name: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         lowercase: true,
+//         validate(value){
+//             if(validator.isEmail(value)){
+//                 throw new Error("Email is invalid")
+//             }
+//         }
+//     },
+//     password: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         minlength: 7,
+//         trim: true,
+//         validate(value){
+//             if(value.toLowerCase().includes("password")){
+//                 throw new Error("Password cannot contain password string")
+//             }
+//         }
+//     },
+//     age: {
+//         type: Number,
+//         validate(value){
+//             if(value<0){
+//                 throw new Error("Age must be a positive number!")
+//             }
+//         }
+//     }
+// })
+
+// const me = new User({
+//     name: "  Andrew  ",
+//     email: "MYEMAIL@MEAD.IO  ",
+//     password: "phone098!"
+// })
+
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log("Error", error)
+// })
+
+const Task = mongoose.model("Task", {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const task = new Task({
+    description: "   Eat lunch"
+})
+
+task.save().then(() => {
+    console.log(task)
+}).catch((error) => {
+    console.log(error)
+})
